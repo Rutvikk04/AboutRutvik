71ad778ba8862103f79edb22730b4f3daa25dab5
Rutvikk04 <r.amarcholi@gmail.com>
2025-05-14 01:29:03 +0530

    nav updated

--- src/Main Components/Navbar.jsx
+++ src/Main Components/Navbar.jsx
@@ -1,51 +1,83 @@
 import React, { useState } from "react";
-import { Menu , X } from "feather-icons-react/build/IconComponents"; 
+import { Menu, X } from "feather-icons-react/build/IconComponents";
 const Navbar = () => {
   const [menuSidebar, setMenuSidebar] = useState(false);
-  const menuItems = ["Home", "Projects", "Experience", "About","Contact"];
+  const [activeLink, setActiveLink]= useState("Home")
+  const menuItems = ["Home", "Projects", "Experience", "About", "Contact"];
   return (
     <>
-      <nav className="flex fixed max-md:relative w-screen z-50 justify-between h-16 p-4 bg-black text-white ">
-        <h1 className="text-2xl text-sky-500 font-pacifico ">Rutvik</h1>
-        <ul className="flex space-x-20 flex-row font-semibold  max-sm:hidden max-md:hidden">
-          {menuItems.map((item, index) => {
-            return (
-              <li
-                key={index}
-                onClick={()=>window.location.replace(window.location.href + `#${item}`)}
-                className=" text-gray-400 hover:text-white hover:border-b-2 hover:border-gray-500 cursor-pointer hover:scale-105 duration-100"
-              >
-                <a href={`#${item}`} >{item}</a>
-              
-                
-              </li>
-            );
-          })}
-        </ul>
-        <button className=" max-md:block hidden bg-black hover:border-none hover:outline-none" onClick={()=>setMenuSidebar(!menuSidebar)}>
-          {
-            !menuSidebar ? <Menu/>:<X/> //add icons Here
-          }
-        </button>
+      <nav
+        id="navbar"
+        className="nav-div"
+      >
+        <div className=" mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16 md:h-20">
+            {/* Logo */}
+            <div className="flex-shrink-0 flex items-center">
+              <div className="flex items-center">
+                <span className="text-2xl text-sky-500 font-pacifico ">Rutvik</span>
+              </div>
+            </div>
+            {/* Desktop Menu */}
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-center space-x-8">
+                {
+                  menuItems.map(menu => (
+                    <a
+                      href={`#${menu}`}
+                      onClick={()=>setActiveLink(menu)}
+                      className={`nav-link  hover:text-blue-600 px-1 py-2 text-sm font-medium ${menu===activeLink ? "active text-blue-600" :"text-gray-500"}`}
+                    >
+                      
+                    {menu}
+                    </a>
+
+                  ))
+                }
+      
+              </div>
+            </div>
+            {/* CTA and Mobile Menu Button */}
+              <div className="md:hidden">
+                <button
+                  id="mobile-menu-button"
+                  onClick={() => setMenuSidebar(prev => !prev)}
+                  className="text-gray-700 hover:text-blue-600 focus:outline-none"
+                >
+                  {
+                    menuSidebar ? <X /> :
+                      <Menu />
+                  }
+                </button>
+              </div>
+          </div>
+        </div>
+        {/* Mobile Menu */}
+        {
+          menuSidebar &&
+          <div
+            id="mobile-menu"
+            className=" md:hidden  bg-white border-t border-blue-50"
+          >
+            <div className="px-2 pt-2 pb-3 flex flex-col space-y-1 sm:px-3">
+                  {
+                  menuItems.map(menu => (
+                    <a
+                      href={`#${menu}`}
+                      onClick={()=>setActiveLink(menu)}
+                      className={`nav-link text-gray-700 hover:text-blue-600 px-1 py-2 text-sm font-medium ${menu===activeLink ? "active" :""}`}
+                    >
+                      
+                    {menu}
+                    </a>
+
+                  ))
+                }
+            </div>
+          </div>
+        }
       </nav>
 
-      {menuSidebar && (
-        <div className="max-md:block z-50 hidden ">
-          <ul className=" transition-opacity text-lg h-screen flex flex-col justify-evenly py-3  space-y-3 bg-gradient-to-b from-black to-gray-900 text-center text-white">
-            {menuItems.map((item, index) => {
-              return (
-                <li
-                  key={index}
-                  className="text-white"
-                  onClick={()=>setMenuSidebar(false)}
-                >
-                   <a href={`#${item}`} >{item}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      )}
     </>
   );
 };
